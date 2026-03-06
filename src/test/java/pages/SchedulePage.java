e78af5d48559c877faf8a25c0b8aca0437bda501
Aigerim Artykova <artykova1994@gmail.com>
2026-03-06 00:02:32 -0600

    add schedule tests

--- src/test/java/pages/SchedulePage.java
+++ src/test/java/pages/SchedulePage.java
@@ -1,4 +1,85 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
 public class SchedulePage {
+
+        public SchedulePage(){
+            PageFactory.initElements(Driver.getDriver(), this);
+        }
+
+        // ----------- View existing event -----------
+
+        @FindBy(css = "div.fc-daygrid-day-events")
+        public WebElement dayEventsContainer;
+
+        @FindBy(xpath = "//a[contains(@class,'fc-daygrid-event') and contains(.,'math')]")
+        public WebElement mathEvent;
+
+        @FindBy(xpath = "//aside")
+        public WebElement eventDetailsPanel;
+
+        @FindBy(xpath = "//aside//*[contains(text(),'March')]")
+        public WebElement detailsTitle;
+
+        @FindBy(xpath = "//aside//*[contains(text(),'Publish')]")
+        public WebElement detailsPublishStatus;
+
+        @FindBy(xpath = "//aside//*[contains(text(),'09:00') and contains(text(),'10:00')]")
+        public WebElement detailsTime;
+
+
+        // ----------- Create event -----------
+
+        @FindBy(xpath = "//button[contains(.,'Create event')]")
+        public WebElement createBtn;
+
+        @FindBy(id = "title")
+        public WebElement nameInput;
+
+        @FindBy(id = "startDate")
+        public WebElement dateInput;
+
+        public  void  selectTime(String timeText){
+            Driver.getDriver().findElement(By.xpath("//ul[@role='listbox']//*[normalize-space()='" + timeText + "']")).click();
+
+        }
+
+        public  void setStartTime(String timeText){
+            startTime.click();
+            selectTime(timeText);
+        }
+        public  void setEndTime(String timeText){
+            endTime.click();
+            selectTime(timeText);
+        }
+
+
+        @FindBy(id = "startTime")
+        public WebElement startTime;
+
+        @FindBy(id = "endTime")
+        public WebElement endTime;
+
+        @FindBy(xpath = "//*[contains(text(),'Coding lesson')]")
+        public WebElement createdEvent;
+
+        @FindBy(xpath = "//label[normalize-space()='For whom']/following::div[@role='button'][1]")
+        public WebElement forWhomDropdown;
+
+        @FindBy(xpath = "(//ul[@role='listbox']//li)[1]")
+        public WebElement firstPersonCheckbox;
+
+        @FindBy(xpath = "//button[normalize-space()='Save']")
+        public WebElement saveBtn;
+
+        @FindBy(xpath = "//button[normalize-space()='Publish']")
+        public WebElement publishBtn;
+
+
+
 }
--- src/test/java/tests/ScheduleTests.java
+++ src/test/java/tests/ScheduleTests.java
@@ -1,4 +1,102 @@
 package tests;
 
-public class ScheduleTests {
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.MainPage;
+import pages.SchedulePage;
+import utils.BaseUI;
+import utils.ConfigurationReader;
+import utils.Driver;
+
+public class ScheduleTests  extends  BaseUI{
+    MainPage mainPage;
+    LoginPage loginPage;
+    SchedulePage schedulePage;
+
+    @BeforeMethod(alwaysRun = true)
+    public  void setUp() throws  InterruptedException{
+        Driver.getDriver();
+
+        mainPage = new MainPage();
+        loginPage = new LoginPage();
+        schedulePage = new SchedulePage();
+
+        loginPage.loginWithCorrectCredentials(
+                ConfigurationReader.getProperty("username"),
+                ConfigurationReader.getProperty("password"));
+        waitAndClick(mainPage.schedule);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public  void tearDown(){
+        Driver.closeDriver();
+    }
+
+    @Test(groups = {"schedule","smoke"}, priority = 1)
+
+    public  void viewExistingEventForSelectedDay(){
+
+        waitAndClick(schedulePage.mathEvent);
+        Assert.assertTrue(schedulePage.eventDetailsPanel.isDisplayed(),
+                "Event details panel is not displayed");
+    }
+
+    @Test(groups = {"schedule", "regression"},priority = 2,
+    dependsOnMethods = "viewExistingEventForSelectedDay")
+    public  void createNewEvent(){
+        waitAndClick(schedulePage.createBtn);
+        schedulePage.nameInput.sendKeys("Coding lesson");
+        waitAndClick(schedulePage.forWhomDropdown);
+        waitAndClick(schedulePage.firstPersonCheckbox);
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.ESCAPE).perform();
+        schedulePage.dateInput.clear();
+        schedulePage.dateInput.sendKeys("05.03.26");
+        waitUntilClickable(10,schedulePage.startTime);
+        schedulePage.startTime.click();
+        schedulePage.startTime.sendKeys(Keys.COMMAND+"a");
+        schedulePage.startTime.sendKeys(Keys.DELETE);
+        schedulePage.startTime.sendKeys("0900");
+        schedulePage.startTime.sendKeys(Keys.TAB);
+
+        waitUntilClickable(10,schedulePage.endTime);
+        schedulePage.endTime.click();
+        schedulePage.endTime.sendKeys(Keys.COMMAND+"a");
+        schedulePage.endTime.sendKeys(Keys.DELETE);
+        schedulePage.endTime.sendKeys("1000");
+        schedulePage.endTime.sendKeys(Keys.TAB);
+
+       waitUntilClickable(10,schedulePage.publishBtn);
+
+
+        Assert.assertTrue(schedulePage.publishBtn.isEnabled(),
+                "Publish should be enabled after filling required fields");
+
+
+        waitAndClick(schedulePage.publishBtn);
+        waitUntilClickable(10,schedulePage.createBtn);
+        Assert.assertTrue(schedulePage.createBtn.isDisplayed(),
+                "Created event is not visible in calendar");
+
+    }
+
+
+    @Test(groups = {"schedule", "regression"}, priority = 3)
+    public  void createEventWithoutRequiredFields(){
+        waitAndClick(schedulePage.createBtn);
+
+        Assert.assertFalse(schedulePage.saveBtn.isEnabled(),
+                "Save button should be disabled when required fields are empty");
+
+        Assert.assertFalse(schedulePage.publishBtn.isEnabled(),
+                "Publish button should be disabled when required fields are empty");
+    }
+
+
+
 }
